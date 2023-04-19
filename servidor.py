6f43e0bcfede49a2fd7b802e5abf44d4cac79803
Rafaella <faellanunes01@gmail.com>
2023-04-18 21:14:06 -0300

    (wip) feat: implementa conceito de threads

--- servidor.py
+++ servidor.py
@@ -1,7 +1,35 @@
 import socket
+import threading
 
-HOST = '' # escuta em todas as interfaces de rede
+max_connections = 10
+
+HOST = 'localhost'
 PORT = 5000
+
+def handle_client(conexao):
+    while True:
+        data = conexao.recv(1024).decode() # receber a mensagem
+        if not data:
+            conexao.close()
+        if data.endswith('s'):
+            conexao.send('Mensagem recebida com sucesso!'.encode()) # enviar a confirmação
+        else:
+            conexao.send('Mensagem não terminada em "s". Tente novamente.'.encode()) # enviar mensagem de erro
+    conexao.close() # fechar a conexão
+
+
+def servidor():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # criar o socket do servidor
+    server_socket.bind((HOST, PORT)) 
+    server_socket.listen(max_connections) 
+    print(f'Servidor iniciado em {HOST}:{PORT}')
+
+    while True:
+        conexao, endereco = server_socket.accept() # aceitar uma nova conexão
+        print(f'Nova conexão recebida de {endereco}')
+        thread = threading.Thread(target=handle_client, args=(conexao,)) # criar uma nova thread para lidar com a conexão
+        thread.start() # iniciar a thread
+
 
 def tratar_mensagem(mensagem):
     mensagem = mensagem.strip() # remove espaços em branco no início e fim da mensagem
