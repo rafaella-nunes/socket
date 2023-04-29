730b059138c9774f2bebc087e2d6a51d35e831ed
Rafaella <faellanunes01@gmail.com>
2023-04-29 16:49:20 -0300

    chore: muda metodo main do servidor

--- servidor.py
+++ servidor.py
@@ -6,38 +6,25 @@ clientes = []
 HOST = 'localhost'
 PORT = 5000
 
-def handle_client(conexao):
-    while True:
-        data = conexao.recv(1024).decode() # receber a mensagem
-        if not data:
-            conexao.close()
-        if data.endswith('s'):
-            conexao.send('Mensagem recebida com sucesso!'.encode()) # enviar a confirmação
-        else:
-            conexao.send('Mensagem não terminada em "s". Tente novamente.'.encode()) # enviar mensagem de erro
-    conexao.close() # fechar a conexão
-
+clients = []
 
 def servidor():
 
     servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        servidor.bind(('localhost', 7777))
+        servidor.bind((HOST, PORT))
         servidor.listen()
-
     except:
         return print('\nNão foi possível iniciar o servidor!\n')
 
     while True:
         cliente, endereco = servidor.accept()
-        print(f'Nova conexão recebida de {endereco}')
         clientes.append(cliente)
 
         thread = threading.Thread(target=tratar_mensagem, args=[cliente])
         thread.start()
-
-
+        
 
 def tratar_mensagem(mensagem):
     mensagem = mensagem.strip() # remove espaços em branco no início e fim da mensagem
