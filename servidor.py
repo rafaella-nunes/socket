10d21cacb46ec578ae5a07228f0c6236610e3b37
Rafaella <faellanunes01@gmail.com>
2023-04-29 16:59:20 -0300

    chore: implementa servidor com threads

--- servidor.py
+++ servidor.py
@@ -6,7 +6,6 @@ clientes = []
 HOST = 'localhost'
 PORT = 5000
 
-clients = []
 
 def servidor():
 
@@ -24,31 +23,33 @@ def servidor():
 
         thread = threading.Thread(target=tratar_mensagem, args=[cliente])
         thread.start()
-        
 
-def tratar_mensagem(mensagem):
-    mensagem = mensagem.strip() # remove espaços em branco no início e fim da mensagem
-    if mensagem.endswith('s'): # verifica se a mensagem termina em "s"
-        return mensagem
-    else:
-        return None
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as servidor:
-    servidor.bind((HOST, PORT))
-    servidor.listen()
-    print(f"Servidor ouvindo em {HOST}:{PORT}")
-
-    conexao, endereco_cliente = servidor.accept()
-    print(f"Conexão estabelecida com {endereco_cliente}")
-
-    with conexao:
-        while True:
-            dados = conexao.recv(1024) # recebe até 1024 bytes de dados
-            mensagem = dados.decode('utf-8')
-            mensagem_valida = tratar_mensagem(mensagem)
-            if mensagem == 'tchau':
-                break
-            elif mensagem_valida:
-                print(f"Nova mensagem recebida: {mensagem_valida}")
+def tratar_mensagem(cliente):
+    while True:
+        try:
+            mensagem = cliente.recv(4096)
+            if mensagem != 'sair':
+                broadcast(mensagem, cliente)
             else:
-                print(f"Mensagem inválida: {mensagem}")
+                apagar_cliente(cliente)
+        except:
+            apagar_cliente(cliente)
+            break
+
+
+def broadcast(mensagem, cliente):
+    for clienteItem in clientes:
+        if clienteItem != cliente:
+            try:
+                clienteItem.send(mensagem)
+            except:
+                apagar_cliente(clienteItem)
+
+# remove cliente da lista
+
+
+def apagar_cliente(cliente):
+    clientes.remove(cliente)
+    socket.close(cliente)
+    return 'conexão encerrada'
